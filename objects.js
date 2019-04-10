963da7fc4e7129adc2aac75841580e05cc6dff4b
Patrick B Brown <pbbrown1112@gmail.com>
2019-04-10 17:43:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Beach House: 'Black Car'};
+var playlist = { 'Beach House': 'Black Car'};
 
 function updatePlaylist (playlist, 'artistName', 'songTitle') {
   playlist['artistName'] = 'songTitle'
