4956f954f2c414b3dbef9d1a3194d8bb2ad5c99f
Patrick B Brown <pbbrown1112@gmail.com>
2019-04-10 17:57:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 };
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
 }
